227add5682502a930b2907fa2fae4918d4800b0c
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-11 12:51:26 +0000

    Finalizado los componentes, pero me da muchos errores, a ver cómo soluciono eesto

---
+++ src/js/component/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
+			<strong>Reading Blog</strong> <i className="fab fa-jedi-order" /> by{" "}
+			<a href="http://www.4geeksacademy.com">Yolanda García Barranco</a>
+		</p>
+	</footer>
+);
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navbar } from "../component/Navbar.js";
 import { Jumbotron } from "../component/Jumbotron.js";
+import { Card } from "../component/Card.js";
+import { Footer } from "../component/Footer.js"
 
 //create your first component
 export function Home() {
@@ -11,10 +13,22 @@ export function Home() {
         title=
         text=
         botton=
+        
         />
 
-        
+        <div className="container-card d-flex">
+
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+
+
     </div>
+
+    <Footer />
     
 
     );
