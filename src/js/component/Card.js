f878b9df6d1836eefb0c6fd4b0a8c82b9178c39c
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-18 11:24:27 +0000

    hay que cambiar el props image y los css

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -3,16 +3,16 @@ import PropTypes from "prop-types";
 
 export function Card(props) {
 	return (
-		<div className="card" style={{ width: "35rem", height: "35rem" }}>
+		<div className="card">
 			<img
 				className="card-img-top"
-				src=""
+				src="https://th.bing.com/th/id/Ra16d96e871ffd7289c879d9224943aa7?rik=fJKEWwyg5WEPpg&pid=ImgRaw"
 			/>
 			<div className="card-body">
 				<h5 className="card-title text-center">{props.title}</h5>
 				<p className="card-text text-center">{props.text}</p>
 				<div className="d-flex justify-content-center">
-					<button className="btn btn-primary">{props.botton}</button>
+					<button className="btn btn-danger">{props.botton}</button>
 				</div>
 			</div>
 		</div>
@@ -22,6 +22,7 @@ Card.propTypes = {
 	title: PropTypes.string,
 	text: PropTypes.string,
 	botton: PropTypes.string
+	//image: PropTypes.string
 };
 
 export default Card;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,36 +2,43 @@ import React from "react";
 import { Navbar } from "../component/Navbar.js";
 import { Jumbotron } from "../component/Jumbotron.js";
 import { Card } from "../component/Card.js";
-import { Footer } from "../component/Footer.js"
+import { Footer } from "../component/Footer.js";
+import "../../../src/styles/card.scss";
 
 //create your first component
 export function Home() {
-    return (
-    <Navbar />
-    <div className="jumbotron-container">
-        <Jumbotron 
-        title=
-        text=
-        botton=
-        
-        />
+	return (
+		<>
+			<Navbar />
 
-        <div className="container-card d-flex">
+			<div className="jumbotron-container">
+				<Jumbotron
+					title="A Warm Welcome!"
+					text="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen."
+					botton="Call to action!"
+				/>
 
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+				<div className="container-card d-flex">
+					<Card
+						title="Ursula K. Le Guin and The Tales of Earthsea"
+						text="The tales of this book explore and extend the world established by the Earthsea novels, written by Ursula K. Le Guin.
+                        A world of wonder and magic; where right and wrong is not that easy to discern."
+						botton="Find Out More!"
+					/>
+					<Card
+						title="Leigh Bardugo and Shadow & Bone"
+						text="Leigh Bardugo is the author of the Grishaverse  which spans the Shadow and Bone, the Six of Crows, The Language of Thorns, and King of Scars. Her other works include Wonder Woman: Warbringer and Ninth House."
+						botton="Find Out More!"
+					/>
+					<Card
+						title="Iria G. Parente & Selene M. Pascual"
+						text="Best spanish authors of YA fantasy. Their novels not only outstand for their narrative and intricatre plots, but also for their diverse and well represented LGTB+ community."
+						botton="Find Out More!"
+					/>
+				</div>
+			</div>
 
-        </div>
-
-
-    </div>
-
-    <Footer />
-    
-
-    );
-    
-
-};
+			<Footer />
+		</>
+	);
+}
